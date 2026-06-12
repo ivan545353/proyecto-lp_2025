585ae26aaf4dbe3683b4c9cffca16ce439871ec1
ivan5453 <ivan545353@gmail.com>
2026-06-12 14:04:17 -0300

    FASE 1 TERMINADA

--- app/config/AppConfig.php
+++ app/config/AppConfig.php
@@ -1,18 +1,15 @@
 <?php
 
-const APP_URL = 'http://localhost/lab_prog_2025_reales_ivan/public/';
-define('APP_URI', $_SERVER['DOCUMENT_ROOT'] . '/lab_prog_2025_reales_ivan/app/');
+const APP_URL = 'http://localhost/lab_prog_2025_reales_ivan_backend/public/';
+define('APP_URI', $_SERVER['DOCUMENT_ROOT'] . '/lab_prog_2025_reales_ivan_backend/app/');
 
-define('APP_DIR_TEMPLATE',  APP_URI . 'resources/template/');
-define('APP_DIR_VIEWS',     APP_URI . 'resources/views/');
-define('APP_DIR_REPORTS',   APP_URI . 'resources/reports/');
 
-define('APP_FILE_TEMPLATE',     APP_DIR_TEMPLATE . 'template.php');
+
 define('APP_FILE_LOG_ERRORS',   APP_URI.'logs/error.log');
 define('APP_FILE_LOG_ACCESS',   APP_URI.'logs/access.log');
 
-define('APP_FILE_LOGIN',    APP_DIR_VIEWS . 'authentication/index.php');
-define('APP_FILE_LOGOUT',   APP_DIR_VIEWS . 'authentication/logout.php');
+
+
 
 
 //#########################################
